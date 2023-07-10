9f34a00527f3bd6c6df4c163f29981ceb6b311ab
vshivtsevnikolai <101023538+vshivtsevnikolai@users.noreply.github.com>
2023-07-10 23:25:19 +0300

    Add files via upload

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,14 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    private Deth8 death;
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( ";
+    }
+}
---
+++ src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,16 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Rabbit4 {
+
+    private Duck5 duck;
+
+    @Override
+    public String toString() { return ", в зайце утка " + duck.toString(); }
+
+    @Autowired
+    public void setDuck(Duck5 duck) { this.duck = duck; }
+}
